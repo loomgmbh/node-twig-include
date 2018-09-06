6a3a101e61b1c11884478e540a79b57883cf22d3
p.sagitza <p.sagitza@loom.de>
2018-09-06 11:34:10 +0200

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = class TwigInclude {
+
+  static extend(Twig) {
+    Twig.functions.include = function(template, vars) {
+      var path = Twig.path.parsePath(this, template);
+      var sub_template = Twig.Templates.loadRemote(path, {
+        id: this.id,
+        method: this.getLoaderMethod(),
+        async: false,
+        path: path,
+        options: this.options,
+      });
+
+      return sub_template.renderAsync(vars);
+    };
+  }
+
+}
